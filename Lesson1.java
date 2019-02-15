9f99e72e25af22d41be7977d3fb49140eb114c08
Gavrilita Victor <victor.gavrilita@endava.com>
2019-02-15 11:15:06 +0200

    lesson 1 and 2

--- Lesson1.java
+++ Lesson1.java
@@ -1,8 +1,30 @@
 public class Lesson1 {
-        public static void main(String []args){
+    public static void main(String[] args) {
+        int min = 2;
+        int max = 1000;
+        int prime = 0;
 
-            System.out.println("Hello");
+        int i;
+        int j;
 
+        System.out.println("Prime numbers in range " + min + " - " + max + " are following:");
+
+        for (i = min; i <= max; i++) {
+
+            for (j = 2; j < i; j++) {
+
+                if (i % j == 0) {
+                    prime = 0;
+                    break;
+                } else {
+                    prime = 1;
+                }
+            }
+            if (prime == 1) {
+                System.out.println(i);
+            }
 
         }
+
+    }
 }
