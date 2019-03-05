f909f4a98e90ff7b39f86d80f41c7e5a2f0a0bd6
Gavrilita Victor <victor.gavrilita@endava.com>
2019-03-05 14:21:55 +0200

    lesson 1-5

--- Lesson3.java
+++ Lesson3.java
@@ -1,6 +1,46 @@
 
 public class Lesson3 {
 
+    public static int getNumberOfDigit(int number) {
+        int initialNumber = number, numberOfdigits = 0;
+
+        while (initialNumber != 0) {
+            initialNumber /= 10;
+            ++numberOfdigits;
+        }
+        return numberOfdigits;
+    }
+
+    public static boolean isPalindromeRecursive(int number) {
+        if (getNumberOfDigit(number) <= 1) {
+            return true;
+        }
+
+        int lastNumber = number % 10;
+        int firstNumber = (int) (number / Math.pow(10, getNumberOfDigit(number) - 1));
+
+
+        int newnumber = number / 10;
+
+
+        newnumber = (int) (newnumber % Math.pow(10, getNumberOfDigit(newnumber) - 1));
+        System.out.println(String.format("Number: %d, Last digit: %d, First digit: %d, New number: %s", number, lastNumber, firstNumber, newnumber));
+
+        return lastNumber == firstNumber && isPalindromeRecursive(newnumber);
+
+
+    }
+    //-----------------------------------------------------------------------------
+
+    public static boolean isPalindromeString(String text) {
+
+        String textrev = new StringBuilder(text).reverse().toString();
+        System.out.println(textrev);
+        return text.equals(textrev);
+
+    }
+    //-----------------------------------------------------------------------------
+
     public static boolean palindrome(int number) {
 
         boolean palindrome = false;
@@ -21,7 +61,7 @@ public class Lesson3 {
             splitednumber = number % 10;
             reverse = (reverse * 10) + splitednumber;
             number = number / 10;
-            System.out.println(reverse);
+            //System.out.println(reverse);
         }
 
 
@@ -33,10 +73,12 @@ public class Lesson3 {
 
 
     public static void main(String args[]) {
-        int number = 22522;
-        if (palindrome(number)) {
-            System.out.println("Number " + number + " is palindrome");
-        }
+        int number = 1001001;
+        String text = "lalalal";
+        System.out.println(isPalindromeString(text));
+//        if (palindrome(number)) {
+//            System.out.println("Number " + number + " is palindrome");
+//        }
 
 
     }
--- Lesson5.java
+++ Lesson5.java
@@ -1,5 +1,8 @@
 public class Lesson5 {
 
+    //-----------------------------------------------------------------------------
+    //Fibonaci Loop
+
     public static int fibonacciLoop(int fibnumber) {
         int i;
         int nr1 = 0;
@@ -15,11 +18,37 @@ public class Lesson5 {
         }
         return fibonaccinr;
     }
+    //-----------------------------------------------------------------------------
+    //Fibonacci recursive
 
-//    public static int fibonacciRecurs(int fibnumber){
+    public static int fibonacciRecurs(int fibnumber) {
+        int i;
+        int nr1 = 0;
+        int nr2 = 1;
+        int fibonaccinr = 0;
+
+        if (fibnumber == 0) {
+            return 0;
+        } else if (fibnumber == 1 || fibnumber == 2) {
+            return 1;
+
+        } else {
+            System.out.println();
+            return fibonacciRecurs(fibnumber - 1) + fibonacciRecurs(fibnumber - 2);
+        }
+
+
+//        if (fibnumber >= 1) {
+//            fibonaccinr = nr1 + nr2;
+//            //System.out.println(fibonaccinr);
+//            nr1 = nr2;
+//            nr2 = fibonaccinr;
 //
-//
-//    }
+//        }
+//        return fibonacciRecurs(fibnumber - 1);
+    }
+    //-----------------------------------------------------------------------------
+    // Fibonacci range check
 
     public static int fibonacciRangecheck(int min, int max) {
 
@@ -43,18 +72,20 @@ public class Lesson5 {
 
             if (fibonaccinr >= min && fibonaccinr <= max) {
                 count++;
-                System.out.println(fibonaccinr);
+                //System.out.println(fibonaccinr);
             }
         }
         return count;
     }
 
+    //-----------------------------------------------------------------------------
     public static void main(String args[]) {
 
         long start = System.currentTimeMillis();
 
-        //System.out.println(fibonacciLoop(500));
-        System.out.println(fibonacciRangecheck(1, 1000000));
+        //System.out.println(fibonacciLoop(10));
+        System.out.println(fibonacciRecurs(10));
+        //System.out.println(fibonacciRangecheck(1, 1000000));
 
         long end = System.currentTimeMillis();
         long difference = (end - start);
